b1fdf46b8478437e43b9261117123c56afa6bc68
D4D3v4l <diegocollantes202010@gmail.com>
2025-11-07 00:36:26 -0500

    .

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://D4D3v4l.github.io/",
+  base: "astro-git-pages",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
