f25221776d5820f099f4935421637321b35d3005
D4D3v4l <diegocollantes202010@gmail.com>
2025-11-07 12:55:55 -0500

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,4 +17,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [vue()],
 });
